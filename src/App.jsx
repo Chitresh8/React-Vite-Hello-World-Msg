bff7d65da84f864608c18e762307e13e563bc7f4
Chitresh8 <123091567+Chitresh8@users.noreply.github.com>
2023-08-26 21:14:31 +0530

    React-Vite
    Using React,Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        <h1>Hello World</h1>
+        <p>Welcome to your React app using Vite!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
